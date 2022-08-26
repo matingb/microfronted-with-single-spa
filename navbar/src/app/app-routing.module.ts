7538dff08b7f1877f12b8c56e0b3f438a72f7c38
Matías García <mgarcia@inrule.com>
2022-08-26 09:34:44 -0300

    set up aplication

---
+++ navbar/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ]
+})
+export class AppRoutingModule { }
---
+++ navbar/src/main.single-spa.ts
@@ -0,0 +1,37 @@
+import { enableProdMode, NgZone } from '@angular/core';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Router, NavigationStart } from '@angular/router';
+
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  Router,
+  NavigationStart,
+  NgZone,
+  domElementGetter: () => {
+    let htmlElement = document.getElementById("navbar")
+    if(htmlElement == undefined ||htmlElement == null){
+      htmlElement = document.createElement("div");
+    }
+    return htmlElement;
+  }
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
